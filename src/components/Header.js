580f8550b7a283dbf3bc32d7628c9b560910ce36
Cody Cooper <cody_codster@live.com>
2023-03-08 05:28:49 -0600

    Installed tailwind and created header

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Header() {
+    return (
+        <>
+        <div className='typewriter'>
+            <h1 className="flex justify-center">TIC-TAC-REACT</h1>
+        </div>
+        </>
+    )
+}
